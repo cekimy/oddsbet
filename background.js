9b1a16339c56f0786ab2ef4484e1ad81fe4c07e6
coco <yankai@xuetangx.com>
2019-02-12 10:43:44 +0800

    增加网址

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status !== 'complete') return false;
-    if (tab.url.indexOf('http://vip.win0168.com/1x2/oddslist/') > -1) {
+    if (tab.url.indexOf('http://vip.win0168.com/1x2/oddslist/') > -1 || tab.url.indexOf('http://op1.win007.com/oddslist/') > -1) {
         chrome.tabs.executeScript(null, {file: "content_script.js"});
     }
 });
